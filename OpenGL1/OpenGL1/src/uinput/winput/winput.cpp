89b45a141a7d7b2d0b686ce0a9517cbfb4ec9b28
Kieran Mason <me@kieranmason.co.uk>
2018-12-28 20:26:15 +0000

    Initial commit

--- OpenGL1/OpenGL1/src/uinput/winput/winput.cpp
+++ OpenGL1/OpenGL1/src/uinput/winput/winput.cpp
@@ -3,9 +3,12 @@
 #include <cmath>
 
 void WInput::update(long long deltaTime) {
+	// Calculate the rate of interpolation
 	float interpolationRate = ((float) deltaTime) / interpolate;
+	// Determine direction from Windows keypresses (X axis)
 	float rawInput = (float) (((GetKeyState('D') & 0x8000) ? 1 : 0) - ((GetKeyState('A') & 0x8000) ? 1 : 0));
 
+	// If there's no direction (or input cancelled out), begin returning the joystick to the original position
 	if (rawInput == 0) {
 		if (xInput < 0) {
 			xInput = clampf(xInput + interpolationRate, -1.0f, 0.0f);
@@ -14,12 +17,15 @@ void WInput::update(long long deltaTime) {
 			xInput = clampf(xInput - interpolationRate, 0.0f, 1.0f);
 		}
 	}
+	// Otherwise, move joystick based on X input
 	else {
 		xInput = clampf(xInput + rawInput * interpolationRate, -1.0f, 1.0f);
 	}
 
-	rawInput = (float)(((GetKeyState('W') & 0x8000) ? 1 : 0) - ((GetKeyState('S') & 0x8000) ? 1 : 0));
+	// Determine direction again (Y axis)
+	rawInput = (float) (((GetKeyState('W') & 0x8000) ? 1 : 0) - ((GetKeyState('S') & 0x8000) ? 1 : 0));
 
+	// If there's no direction, begin returning the joystick to the original position
 	if (rawInput == 0) {
 		if (yInput < 0) {
 			yInput = clampf(yInput + interpolationRate, -1.0f, 0.0f);
@@ -28,6 +34,7 @@ void WInput::update(long long deltaTime) {
 			yInput = clampf(yInput - interpolationRate, 0.0f, 1.0f);
 		}
 	}
+	// Otherwise, move joystick based on Y input
 	else {
 		yInput = clampf(yInput + rawInput * interpolationRate, -1.0f, 1.0f);
 	}
@@ -35,6 +42,7 @@ void WInput::update(long long deltaTime) {
 	// Calculate distance
 	float dist = sqrtf(powf(xInput, 2) + powf(yInput, 2));
 
+	// Normalise the XY input vector
 	if (dist > 1) {
 		xInput /= dist;
 		yInput /= dist;
